e94645e149969e245f9d40177218e41f7a44b0ac
te87037 <te87037@gmail.com>
2026-08-03 15:59:15 +0800

    Configure Studio for GitHub Pages subpath

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID || "kaili000",
     dataset: process.env.SANITY_STUDIO_DATASET || "production",
   },
+  project: {
+    basePath: process.env.SANITY_STUDIO_BASEPATH || "",
+  },
 });
